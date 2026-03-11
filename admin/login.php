147056354432b644ef8c447e1b1845c558f0858f
Richard-dev10 <elsonpoudel777@gmail.com>
2026-03-11 19:32:53 +0100

    Create basic admin login page structure

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,14 @@
 <?php
 // Admin login page
 ?>
+
+<h1>Admin Login</h1>
+
+<form method="POST">
+
+<input type="text" name="username" placeholder="Enter username"> <br>
+<input type="password" name="password" placeholder="Enter password"> <br>
+
+<button type="submit">Login</button>
+
+</form>
