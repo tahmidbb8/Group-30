4e866268b9d2442a94ec848b272f54126d573a28
Richard-dev10 <elsonpoudel777@gmail.com>
2026-03-11 20:57:09 +0100

    Add basic login form validation

--- admin/login.php
+++ admin/login.php
@@ -1,13 +1,28 @@
 <?php
-// Admin login page
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if (empty($username)) {
+        echo "Username is required<br>";
+    }
+
+    if (empty($password)) {
+        echo "Password is required<br>";
+    }
+
+}
+
 ?>
 
 <h1>Admin Login</h1>
 
 <form method="POST">
 
-<input type="text" name="username" placeholder="Enter username"> <br>
-<input type="password" name="password" placeholder="Enter password"> <br>
+<input type="text" name="username" placeholder="Enter username"><br>
+<input type="password" name="password" placeholder="Enter password"><br>
 
 <button type="submit">Login</button>
 
